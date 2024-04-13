1ebc5133b459c0afe3b4b84e03df3a027191f738
DaCasBe <dacasbe97@gmail.com>
2024-04-13 17:33:00 +0200

    (#110): charity campaigns functions

--- twitchpy/dataclasses/charity_campaign.py
+++ twitchpy/dataclasses/charity_campaign.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from ..dataclasses import Channel, CharityCampaignAmount
+
 
 @dataclass
 class CharityCampaign:
@@ -8,24 +10,20 @@ class CharityCampaign:
 
     Attributes:
         campaign_id (str): An ID that identifies the charity campaign
-        broadcaster_id (str): An ID that identifies the broadcaster that’s running the campaign
-        broadcaster_name (str): The broadcaster’s display name
-        broadcaster_login (str): The broadcaster’s login name
+        channel (Channel): The channel that's running the campaign
         charity_name (str): The charity’s name
         charity_description (str): A description of the charity
         charity_logo (str): A URL to an image of the charity’s logo
         charity_website (str): A URL to the charity’s website
-        current_amount (dict): The current amount of donations that the campaign has received
-        target_amount (dict): The campaign’s fundraising goal
+        current_amount (CharityCampaignAmount): The current amount of donations that the campaign has received
+        target_amount (CharityCampaignAmount): The campaign’s fundraising goal
     """
 
     campaign_id: str
-    broadcaster_id: str
-    broadcaster_name: str
-    broadcaster_login: str
+    channel: Channel
     charity_name: str
     charity_description: str
     charity_logo: str
     charity_website: str
-    current_amount: dict
-    target_amount: dict
+    current_amount: CharityCampaignAmount
+    target_amount: CharityCampaignAmount
--- twitchpy/dataclasses/charity_campaign_donation.py
+++ twitchpy/dataclasses/charity_campaign_donation.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from ..dataclasses import CharityCampaignAmount, User
+
 
 @dataclass
 class CharityCampaignDonation:
@@ -9,15 +11,11 @@ class CharityCampaignDonation:
     Attributes:
         donation_id (str): An ID that identifies the donation
         campaign_id (str): An ID that identifies the charity campaign that the donation applies to
-        user_id (str): An ID that identifies a user that donated money to the campaign
-        user_login (str): The user’s login name
-        user_name (str): The user’s display name
-        amount (dict): The amount of money that the user donated
+        user (User): The user that donated money to the campaign
+        amount (CharityCampaignAmount): The amount of money that the user donated
     """
 
     donation_id: str
     campaign_id: str
-    user_id: str
-    user_login: str
-    user_name: str
-    amount: dict
+    user: User
+    amount: CharityCampaignAmount
