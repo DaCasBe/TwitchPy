af57ed86386b7bf7aab8531d794b0d2229f60107
DaCasBe <dacasbe97@gmail.com>
2024-04-19 18:23:04 +0200

    (#110): searchs functions

--- twitchpy/_api/searchs.py
+++ twitchpy/_api/searchs.py
@@ -1,5 +1,5 @@
 from .._utils import http
-from ..dataclasses import Channel, Game
+from ..dataclasses import Channel, Game, User
 
 
 def search_categories(
@@ -14,10 +14,7 @@ def search_categories(
 
     games = http.send_get_with_pagination(url, headers, params, first, 100)
 
-    return [
-        Game(game["id"], game["name"], box_art_url=game["box_art_url"])
-        for game in games
-    ]
+    return [Game(game["id"], game["name"], game["box_art_url"]) for game in games]
 
 
 def search_channels(
@@ -28,22 +25,15 @@ def search_channels(
         "Authorization": f"Bearer {token}",
         "Client-Id": client_id,
     }
-    params = {}
-    params["query"] = query
-
-    if live_only is not False:
-        params["live_only"] = live_only
+    params = {"query": query, "live_only": live_only}
 
     channels = http.send_get_with_pagination(url, headers, params, first, 100)
 
     return [
         Channel(
-            channel["id"],
-            channel["broadcaster_login"],
-            channel["display_name"],
+            User(channel["id"], channel["broadcaster_login"], channel["display_name"]),
             channel["broadcaster_language"],
-            channel["game_name"],
-            channel["game_id"],
+            Game(channel["game_id"], channel["game_name"]),
             channel["title"],
             channel["tags"],
         )
